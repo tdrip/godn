d4e12578834ff65017bdb2e086098a8825e852ed
will.allen@venafi.com <will.allen@venafi.com>
2020-12-03 17:58:13 +0000

    Update info_test.go

--- pkg/info_test.go
+++ pkg/info_test.go
@@ -7,49 +7,72 @@ import (
 func TestDefaultTopInfo(t *testing.T) {
 	info := NewInfo(DefaultTop)
 	shouldBeTop(info, t)
-	checkName(info, t)
+	hasName(info, t)
+	shouldNotHaveParent(info, t)
 }
 
 func TestTopInfo(t *testing.T) {
 	info := NewInfo("\\")
 	shouldBeTop(info, t)
-	checkName(info, t)
+	hasNoName(info, t)
+	shouldNotHaveParent(info, t)
 }
 
 func TestInfoCustomSeperator(t *testing.T) {
 	info := NewInfoCustomSeperator('/', "/")
 	shouldBeTop(info, t)
-	checkName(info, t)
+	hasNoName(info, t)
+	shouldNotHaveParent(info, t)
 }
 
 func TestInfoCustomTop(t *testing.T) {
 	info := NewInfoCustomTop("\\TOP", "\\TOP")
 	shouldBeTop(info, t)
-	checkName(info, t)
+	hasName(info, t)
+	shouldNotHaveParent(info, t)
 }
 
 func TestInfoCustomTopWithSlash(t *testing.T) {
 	info := NewInfoCustomTop("\\TOP", "\\TOP\\")
 	shouldBeTop(info, t)
-	checkName(info, t)
+	hasName(info, t)
+	shouldNotHaveParent(info, t)
 }
+
+/// CHILD TESTS FROM HERE FORWARD ////
 
 func TestFirstChildInfo(t *testing.T) {
 
 	info := NewInfo("\\TOP\\Child")
 	shouldNotBeTop(info, t)
-	checkName(info, t)
-
+	hasName(info, t)
+	shouldHaveParent(info, t)
 }
+
 func TestFirstChildInfoWithSlash(t *testing.T) {
-
 	info := NewInfo("\\TOP\\Child\\")
-
 	shouldNotBeTop(info, t)
-	checkName(info, t)
+	hasName(info, t)
+	shouldHaveParent(info, t)
 
 }
 
+func TestSecondChildInfoWithSlash(t *testing.T) {
+	info := NewInfo("\\TOP\\Child\\NewChild\\")
+	shouldNotBeTop(info, t)
+	hasName(info, t)
+	shouldHaveParent(info, t)
+
+}
+func TestSecondChildInfo(t *testing.T) {
+	info := NewInfo("\\TOP\\Child\\NewChild")
+	shouldNotBeTop(info, t)
+	hasName(info, t)
+	shouldHaveParent(info, t)
+
+}
+
+// common functions
 func shouldNotBeTop(info *Info, t *testing.T) {
 	if info.IsTop() {
 		t.Errorf("%s has IsTop() set to true - should be false", info.String())
@@ -64,9 +87,30 @@ func shouldBeTop(info *Info, t *testing.T) {
 	}
 }
 
-func checkName(info *Info, t *testing.T) {
+func hasName(info *Info, t *testing.T) {
 	if len(info.Name) == 0 {
 		t.Errorf("%s does not have a name set", info.String())
+		PrintDetails(info)
+	}
+}
+
+func hasNoName(info *Info, t *testing.T) {
+	if len(info.Name) > 0 {
+		t.Errorf("%s does  have a name set when it should not", info.String())
+		PrintDetails(info)
+	}
+}
+
+func shouldHaveParent(info *Info, t *testing.T) {
+	if info.Parent == nil {
+		t.Errorf("%s does not have a parent when it should", info.String())
+		PrintDetails(info)
+	}
+}
+
+func shouldNotHaveParent(info *Info, t *testing.T) {
+	if info.Parent != nil {
+		t.Errorf("%s does have a parent when it should not", info.String())
 		PrintDetails(info)
 	}
 }
