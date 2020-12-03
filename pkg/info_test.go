1ae9dbfdf7eac5c8c69de560cbd64d102d7a0871
will.allen@venafi.com <will.allen@venafi.com>
2020-12-03 13:20:22 +0000

    Update info_test.go

--- pkg/info_test.go
+++ pkg/info_test.go
@@ -6,41 +6,67 @@ import (
 
 func TestDefaultTopInfo(t *testing.T) {
 	info := NewInfo(DefaultTop)
-	if !info.IsTop() {
-		t.Errorf("%s has IsTop() set to false - should be true", info.String())
-		PrintDetails(info)
-	}
+	shouldBeTop(info, t)
+	checkName(info, t)
 }
 
 func TestTopInfo(t *testing.T) {
 	info := NewInfo("\\")
-	if !info.IsTop() {
-		t.Errorf("%s has IsTop() set to false - should be true", info.String())
-		PrintDetails(info)
-	}
+	shouldBeTop(info, t)
+	checkName(info, t)
 }
 
 func TestInfoCustomSeperator(t *testing.T) {
 	info := NewInfoCustomSeperator('/', "/")
-	if !info.IsTop() {
-		t.Errorf("%s has IsTop() set to false - should be true", info.String())
-		PrintDetails(info)
-	}
+	shouldBeTop(info, t)
+	checkName(info, t)
 }
 
 func TestInfoCustomTop(t *testing.T) {
-	info := NewInfoCustomTop("\\ROOT", "\\ROOT")
+	info := NewInfoCustomTop("\\TOP", "\\TOP")
+	shouldBeTop(info, t)
+	checkName(info, t)
+}
+
+func TestInfoCustomTopWithSlash(t *testing.T) {
+	info := NewInfoCustomTop("\\TOP", "\\TOP\\")
+	shouldBeTop(info, t)
+	checkName(info, t)
+}
+
+func TestFirstChildInfo(t *testing.T) {
+
+	info := NewInfo("\\TOP\\Child")
+	shouldNotBeTop(info, t)
+	checkName(info, t)
+
+}
+func TestFirstChildInfoWithSlash(t *testing.T) {
+
+	info := NewInfo("\\TOP\\Child\\")
+
+	shouldNotBeTop(info, t)
+	checkName(info, t)
+
+}
+
+func shouldNotBeTop(info *Info, t *testing.T) {
+	if info.IsTop() {
+		t.Errorf("%s has IsTop() set to true - should be false", info.String())
+		PrintDetails(info)
+	}
+}
+
+func shouldBeTop(info *Info, t *testing.T) {
 	if !info.IsTop() {
 		t.Errorf("%s has IsTop() set to false - should be true", info.String())
 		PrintDetails(info)
 	}
 }
 
-func TestNonTopInfo(t *testing.T) {
-
-	info := NewInfo("\\ROOT\\")
-	if info.IsTop() {
-		t.Errorf("%s has IsTop() set to false - should be true", info.String())
+func checkName(info *Info, t *testing.T) {
+	if len(info.Name) == 0 {
+		t.Errorf("%s does not have a name set", info.String())
 		PrintDetails(info)
 	}
 }
