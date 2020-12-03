3d05e029498e8e8530109b5f40c0e6d4851b378e
will.allen@venafi.com <will.allen@venafi.com>
2020-12-03 12:38:52 +0000

    More tests

--- pkg/info_test.go
+++ pkg/info_test.go
@@ -6,25 +6,41 @@ import (
 
 func TestDefaultTopInfo(t *testing.T) {
 	info := NewInfo(DefaultTop)
-	PrintDetails(info)
 	if !info.IsTop() {
 		t.Errorf("%s has IsTop() set to false - should be true", info.String())
+		PrintDetails(info)
 	}
 }
 
 func TestTopInfo(t *testing.T) {
 	info := NewInfo("\\")
-	PrintDetails(info)
 	if !info.IsTop() {
 		t.Errorf("%s has IsTop() set to false - should be true", info.String())
+		PrintDetails(info)
+	}
+}
+
+func TestInfoCustomSeperator(t *testing.T) {
+	info := NewInfoCustomSeperator('/', "/")
+	if !info.IsTop() {
+		t.Errorf("%s has IsTop() set to false - should be true", info.String())
+		PrintDetails(info)
+	}
+}
+
+func TestInfoCustomTop(t *testing.T) {
+	info := NewInfoCustomTop("\\ROOT", "\\ROOT")
+	if !info.IsTop() {
+		t.Errorf("%s has IsTop() set to false - should be true", info.String())
+		PrintDetails(info)
 	}
 }
 
 func TestNonTopInfo(t *testing.T) {
 
 	info := NewInfo("\\ROOT\\")
-	PrintDetails(info)
 	if info.IsTop() {
 		t.Errorf("%s has IsTop() set to false - should be true", info.String())
+		PrintDetails(info)
 	}
 }
